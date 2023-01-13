41058a1d4921f7ad133f6e2e7253636e9e87390d
ohmin <71929898+dhalsdl12@users.noreply.github.com>
2023-01-13 11:59:40 +0900

    Update naver_sports_article.py

--- naver_sports_article.py
+++ naver_sports_article.py
@@ -17,11 +17,14 @@ def pageCrawl():
 
 
 url = 'https://search.naver.com/search.naver?where=news&query=%EB%A7%A8%EC%B2%B4%EC%8A%A4%ED%84%B0%20%EC%8B%9C%ED%8B%B0&sm=tab_opt&sort=0&photo=0&field=0&pd=4&ds=2023.01.12.10.34&de=2023.01.13.10.34&docid=&related=0&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so%3Ar%2Cp%3A1d&is_sug_officeid=0'
-driver = 'chromedriver.exe'
+chrome_driver = os.path.join('chromedriver')
+chrome_options = webdriver.ChromeOptions()
+
 article_title = []
 article_link = []
 file = open("article.txt", "w", encoding="utf-8")
-drive = webdriver.Chrome(driver)
+
+drive = webdriver.Chrome(chrome_driver, options=chrome_options)
 drive.get(url)
 
 pageCrawl()
