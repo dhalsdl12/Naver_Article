a909a21d88313f9fe3e0e6e0ab60c100804b77be
dhalsdl12 <dhalsdl12@gmail.com>
2023-01-13 11:30:56 +0900

    aa

--- naver_sports_article.py
+++ naver_sports_article.py
@@ -17,7 +17,7 @@ def pageCrawl():
 
 
 url = 'https://search.naver.com/search.naver?where=news&query=%EB%A7%A8%EC%B2%B4%EC%8A%A4%ED%84%B0%20%EC%8B%9C%ED%8B%B0&sm=tab_opt&sort=0&photo=0&field=0&pd=4&ds=2023.01.12.10.34&de=2023.01.13.10.34&docid=&related=0&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so%3Ar%2Cp%3A1d&is_sug_officeid=0'
-driver = 'C:\\Users\\dhals\\Downloads\\chromedriver.exe'
+driver = 'chromedriver.exe'
 article_title = []
 article_link = []
 file = open("article.txt", "w", encoding="utf-8")
