0807ec90615fc62e057427a1c37748d74a2b6e4b
dhalsdl12 <dhalsdl12@gmail.com>
2023-01-13 11:20:05 +0900

    make text file

---
+++ naver_sports_article.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def pageCrawl():
+    article_list = drive.find_elements(By.XPATH, "//ul[@class=\"list_news\"]/li")
+    for article in article_list:
+        try:
+            news_info = article.find_element(By.XPATH, "./div/div/a[@class=\"news_tit\"]")
+            title = news_info.get_attribute('title')
+            link = news_info.get_attribute('href')
+            article_title.append(title)
+            article_link.append(link)
+            file.write(title + '\n' + link + '\n\n')
+        except:
+            print("error")
+
+
+url = 'https://search.naver.com/search.naver?where=news&query=%EB%A7%A8%EC%B2%B4%EC%8A%A4%ED%84%B0%20%EC%8B%9C%ED%8B%B0&sm=tab_opt&sort=0&photo=0&field=0&pd=4&ds=2023.01.12.10.34&de=2023.01.13.10.34&docid=&related=0&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so%3Ar%2Cp%3A1d&is_sug_officeid=0'
+driver = 'C:\\Users\\dhals\\Downloads\\chromedriver.exe'
+article_title = []
+article_link = []
+file = open("article.txt", "w", encoding="utf-8")
+drive = webdriver.Chrome(driver)
+drive.get(url)
+
+pageCrawl()
+for i in range(len(article_title)):
+    print(article_title[i])
+    print(article_link[i])
+
+drive.close()
+file.close()
+print('finish')
