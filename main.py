fd5784a1808ca9e604c12b0ff91d394680b587eb
ohmin <71929898+dhalsdl12@users.noreply.github.com>
2023-01-16 22:00:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -36,8 +36,7 @@ def extract_book_data():
 
 
 def execute_drive():
-    url = 'https://search.naver.com/search.naver?where=news&query=%EB%A7%A8%EC%B2%B4%EC%8A%A4%ED%84%B0%20%EC%8B%9C%ED%8B%B0&sm=tab_opt&sort=0&photo=0&field=0&pd=4&ds=2023.01.12.10.34&de=2023.01.13.10.34&docid=&related=0&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so%3Ar%2Cp%3A1d&is_sug_officeid=0'
-
+    url = 'https://search.naver.com/search.naver?where=news&query=%EC%B5%9C%EC%9A%B0%EC%8B%9D&sm=tab_opt&sort=0&photo=0&field=0&pd=4&ds=&de=&docid=&related=0&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so%3Ar%2Cp%3A1d&is_sug_officeid=0'
     chrome_driver = os.path.join('chromedriver')
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
