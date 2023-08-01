238aa1854d0f438725fcf366ba7163b92832fe32
Ohmin Kwon <71929898+dhalsdl12@users.noreply.github.com>
2023-08-01 16:08:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def extract_article_data():
 
 def execute_drive():
     url = 'https://search.naver.com/search.naver?where=news&query=%EB%A7%A8%EC%B2%B4%EC%8A%A4%ED%84%B0%EC%8B%9C%ED%8B%B0&sm=tab_opt&sort=0&photo=0&field=0&pd=4&ds=&de=&docid=&related=0&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so%3Ar%2Cp%3A1d&is_sug_officeid=0'
-    
+    '''
     chrome_driver = os.path.join('chromedriver')
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
@@ -47,6 +47,14 @@ def execute_drive():
     chrome_options.add_argument('--disable-gpu')
     service = ChromeService(executable_path=chrome_driver)
     drive = webdriver.Chrome(service=service, options=chrome_options)
+    drive.get(url)
+    '''
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-gpu')
+    drive = webdriver.Chrome(options=chrome_options)
     drive.get(url)
     return drive
 
